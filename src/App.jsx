ae577d9a5b0ad561a413f73a5c2d1e08ef47a61a
astarta16 <ngedeshuri2001@gmail.com>
2023-10-05 19:00:15 +0400

    add funcionality

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,30 @@ import {
 
 function App() {
   const [rangeValue, setRangeValue] = useState(10);
+  const [strength, setStrength] = useState(0);
+  const [upperCase, setUpperCase] = useState(false);
+  const [lowerCase, setLowerCase] = useState(false);
+  const [number, setNumber] = useState(false);
+  const [symbol, setSymbol] = useState(false);
+  const [password, setPassword] = useState("");
+
+  function handleStrength(event, text) {
+    if (text === "Include Uppercase Letters") {
+      setUpperCase(!upperCase);
+    } else if (text === "Include Lowercase Letters") {
+      setLowerCase(!lowerCase);
+    } else if (text === "Include Numbers") {
+      setNumber(!number);
+    } else {
+      setSymbol(!symbol);
+    }
+
+    if (event.target.checked) {
+      setStrength((prev) => prev + 1);
+    } else {
+      setStrength((prev) => prev - 1);
+    }
+  }
 
   const checkBoxText = [
     "Include Uppercase Letters",
@@ -31,10 +55,30 @@ function App() {
     "Include Symbols",
   ];
 
-  const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  const lowerCase = "abcdefghijklmnopqrstuvwxyz";
-  const number = "0123456789";
-  const symbol = "@#$%^&*()_+~|}{[]></-=";
+  function generatePassword() {
+    const upperCaseSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const lowerCaseSet = "abcdefghijklmnopqrstuvwxyz";
+    const numberSet = "0123456789";
+    const symbolSet = "@#$%^&*()_+~|}{[]></-=";
+
+    let chars = ""
+    if(upperCase) {
+      chars += upperCaseSet }
+
+      if(lowerCase) {
+        chars += lowerCaseSet
+      }
+
+      if(number) {
+        chars += numberSet
+      }
+
+      if(symbol) {
+        chars += symbolSet
+      }
+     console.log(chars)
+
+  }
 
   return (
     <div>
@@ -59,7 +103,10 @@ function App() {
           <div>
             {checkBoxText.map((text, index) => (
               <CheckboxContainer key={index}>
-                <CheckboxInput type="checkbox" />
+                <CheckboxInput
+                  onChange={(event) => handleStrength(event, text)}
+                  type="checkbox"
+                />
                 <CheckboxLabel>{text}</CheckboxLabel>
               </CheckboxContainer>
             ))}
@@ -74,7 +121,7 @@ function App() {
               <StrengthRectangle />
             </StrengthRectangleContainer>
           </StrengthBlock>
-          <Button>Generate</Button>
+          <Button onClick={generatePassword}>Generate</Button>
         </SecondCard>
       </CardContainer>
     </div>
